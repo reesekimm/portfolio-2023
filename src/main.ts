c3e17606b1e8d87fad2202bbe6d7ff21568b0fd1
reesekimm <reesekimm@gmail.com>
2023-05-11 17:31:38 +0900

    about css + animation 구현

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,48 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.set('.page', { position: 'absolute' })
+const pages = gsap.utils.toArray('.page')
 
-gsap.to('.page', {
-  xPercent: '-100',
-  stagger: 0.8,
+const aboutMarquee = document.querySelector('.about__marquee')
+const aboutContainer = document.querySelector('.about__container')
+const aboutItems = document.querySelectorAll('.about__item')
+
+const pageScrollTween = gsap.to(pages, {
+  xPercent: -100,
+  stagger: 0.6,
+  ease: 'none',
   scrollTrigger: {
-    horizontal: true,
     trigger: '.main-container',
-    markers: true,
     end: '+=550%',
     scrub: 1,
     pin: true,
+    markers: true,
+  },
+})
+
+gsap.set(aboutMarquee, { x: -10 })
+gsap.to(aboutMarquee, {
+  xPercent: -120,
+  scrollTrigger: {
+    containerAnimation: pageScrollTween,
+    trigger: aboutMarquee,
+    start: 'left left+=22%',
+    scrub: 1,
+    markers: { startColor: 'hotpink', endColor: 'blue' },
+  },
+})
+
+gsap.from(aboutItems, {
+  y: -50,
+  opacity: 0,
+  duration: 1.5,
+  ease: 'elastic',
+  stagger: 0.1,
+  scrollTrigger: {
+    containerAnimation: pageScrollTween,
+    trigger: aboutContainer,
+    start: 'left left+=20%',
+    toggleActions: 'play none none reverse',
+    markers: { startColor: 'pink', endColor: 'purple', indent: 40 },
   },
 })
